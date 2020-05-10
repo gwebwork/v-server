b1b1787a60dfd5b3cc35ea48ef92e9ff387e95e0
gwebwork <297008775@qq.com>
2020-05-10 16:33:42 +0800

    完成商品功能开发

--- v-public/src/main.js
+++ v-public/src/main.js
@@ -7,6 +7,13 @@ import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 import { Message } from 'element-ui'
 import { MessageBox } from 'element-ui'
+import  VueQuillEditor from 'vue-quill-editor'
+// require styles 引入样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+Vue.use(VueQuillEditor)
 Vue.prototype.$message=Message
 import axios from 'axios'
 import TreeTable from 'vue-table-with-tree-grid'
@@ -24,7 +31,16 @@ Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 Vue.component('tree-table',TreeTable)
-
+Vue.filter('dateFormat',function(originVal){
+  const dt=new Date(originVal)
+  const y=dt.getFullYear()
+  const m=(dt.getMonth()+1+'').padStart(2,'0')
+  const d=(dt.getDate()+'').padStart(2,'0')
+  const hh=(dt.getHours()+'').padStart(2,'0')
+  const mm=(dt.getMinutes()+'').padStart(2,'0')
+  const ss=(dt.getSeconds()+'').padStart(2,'0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 new Vue({
   router,
   render: h => h(App)
--- v-public/src/router.js
+++ v-public/src/router.js
@@ -8,6 +8,8 @@ import Roles from './components/power/Roles.vue'
 import Rights from './components/power/Rights.vue'
 import Cate from './components/goods/Cate.vue'
 import Params from './components/params/Params.vue'
+import GoodsList from './components/goods/List.vue'
+import Add from './components/goods/Add.vue'
 Vue.use(Router)
 
 const router = new Router({
@@ -20,7 +22,9 @@ const router = new Router({
         {path:'/rights',component:Rights},
         {path:'/roles',component:Roles},
         {path:'/categories',component:Cate},
-        {path:'/params',component:Params}
+        {path:'/params',component:Params},
+        {path:'/goods',component:GoodsList},
+        {path:'/goods/add',component:Add}
       ]
   
     },
